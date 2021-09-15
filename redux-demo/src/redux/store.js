5a4eb7d10a7e7e1a20d49db140e9b9cc769a2d20
XavierElon1 <xavierh93@hotmail.com>
2021-09-15 18:58:44 -0400

    add root reducer

--- redux-demo/src/redux/store.js
+++ redux-demo/src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux'
 import cakeReducer from './cakes/cakeReducer'
+import iceCreamReducer from './iceCreams/IceCreamReducer'
+import rootReducer from './rootReducer'
 
-const store = createStore(cakeReducer)
+const store = createStore(rootReducer)
+// const store = createStore(cakeReducer)
 
 export default store
