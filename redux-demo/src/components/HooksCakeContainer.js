60b9268fdcdf4e8cc9d4dc11dd93f87675959b93
XavierElon1 <xavierh93@hotmail.com>
2021-09-15 18:23:13 -0400

    add useSelector functionality

--- redux-demo/src/components/HooksCakeContainer.js
+++ redux-demo/src/components/HooksCakeContainer.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 function HooksCakeContainer() {
+    const numOfCakes = useSelector(state => state.numOfCakes)
     return (
         <div>
-            <h2>Num of cakes : </h2>
+            <h2>Num of cakes : {numOfCakes}</h2>
             <button>Buy cake</button>
         </div>
     )
