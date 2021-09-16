69489600be0cd711c05b15828261a02af2a120c4
XavierElon1 <xavierh93@hotmail.com>
2021-09-16 09:41:27 -0400

    add logger

--- redux-demo/src/redux/store.js
+++ redux-demo/src/redux/store.js
@@ -1,9 +1,10 @@
 import { createStore } from 'redux'
+import logger from 'redux-logger'
 import cakeReducer from './cakes/cakeReducer'
 import iceCreamReducer from './iceCreams/IceCreamReducer'
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 // const store = createStore(cakeReducer)
 
 export default store
