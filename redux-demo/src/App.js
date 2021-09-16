92279bf8ff44fec2ff04010573bbfcbf6c3a4a6c
XavierElon1 <xavierh93@hotmail.com>
2021-09-16 10:29:16 -0400

    add action payload

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <HooksCakeContainer />
         <CakeContainer />
+        <NewCakeContainer />
         <IceCreamContainer />
       </div>
     </Provider>
