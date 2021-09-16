11d2e9a2aea2a9b5d05ce6e5cadc22ada02770ab
XavierElon1 <xavierh93@hotmail.com>
2021-09-16 10:14:28 -0400

    add redux tools

--- redux-demo/src/redux/store.js
+++ redux-demo/src/redux/store.js
@@ -1,10 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger'
 import cakeReducer from './cakes/cakeReducer'
 import iceCreamReducer from './iceCreams/IceCreamReducer'
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
 // const store = createStore(cakeReducer)
 
 export default store
