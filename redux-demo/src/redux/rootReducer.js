0f6a0923ec32f594a679d60f4f484920874222d1
XavierElon1 <xavierh93@hotmail.com>
2021-09-16 13:30:27 -0400

    add async functions

--- redux-demo/src/redux/rootReducer.js
+++ redux-demo/src/redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import cakeReducer from './cakes/cakeReducer'
 import iceCreamReducer from './iceCreams/IceCreamReducer'
+import userReducer from './user/userReducer'
 
 const rootReducer = combineReducers({
     cake: cakeReducer,
